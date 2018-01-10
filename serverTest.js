2175465a311a40ebda501f52c0665bfc8d55d07f
Vinayak <vinayakkini101@gmail.com>
2018-01-10 22:54:25 +0530

    added body-parser and mysql code to serverTest.js

--- serverTest.js
+++ serverTest.js
@@ -37,18 +37,28 @@ app.use('/',router);
 app.set('view engine', 'ejs');
 app.use(express.static(path+'/public'));
 
-/*app.use(express.static(path+'/vendor'));
-app.use(express.static('/css'));
-app.use(express.static('/scss'));
-app.use(express.static('/js'));
-*/
 
-/*app.use('/bootstrap', express.static(path+'/vendor'));
-app.use('/bootstrap', express.static(path+'/vendor'));
-app.use('/images', express.static(path+'/css'));
-app.use('/images', express.static(path+'/scss'));
-app.use('/images', express.static(path+'/js'));
-*/
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password:'',
+	database:'BEProject'
+});
+
+connection.connect(function(error){
+	if(!!error){
+		console.log('Error');
+	}else{
+		console.log('Connected');
+	}
+});
+
+
+
 
 router.get('/',function(req,res){
   res.sendFile(path+'/index.html');
@@ -69,6 +79,19 @@ router.get('/syllabusScheme.html',function(req,res){
 router.get('/syllabusModules.html',function(req,res){
   res.sendFile(path+'/syllabusModules.html');
 });
+
+router.post('/syllabusModules.html',function(req,res){
+  res.sendFile(path+'/syllabusModules.html');
+});
+
+
+app.post('/syllabusModules.html',function(req,res){
+  console.log(req.body);
+  //res.sendFile(path+'/syllabusModules.html');
+  //res.write('id is "'+req.body.modulename+'".\n');
+  res.end();
+});
+
 
 app.listen(3000,function(){
   console.log('Server running at Port 3000');
